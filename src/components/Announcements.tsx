2d37b2a9dd8f2c9e7ae8dba379af57afe573ae5a
dogerddddfd <1739988739@qq.com>
2024-09-13 15:52:43 +0800

    update 240913

---
+++ src/components/Announcements.tsx
@@ -0,0 +1,12 @@
+const Announcements = () => {
+   return (
+      <div className='bg-white p-6 rounded-md'>
+         <div className='flex items-center justify-between'>
+            <h1>Announcements</h1>
+            <span>View All</span>
+         </div>
+      </div>
+   )
+}
+
+export default Announcements
