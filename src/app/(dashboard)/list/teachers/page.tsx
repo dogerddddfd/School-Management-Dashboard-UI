809c171f846d50141825372b5c247f7a98d3af1e
dogerddddfd <1739988739@qq.com>
2024-09-25 14:33:47 +0800

    update 240925

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -3,20 +3,12 @@ import Pagination from "@/components/Pagination"
 import Table from "@/components/Table"
 import TableSearch from "@/components/TableSearch"
 import { role, teachersData } from "@/lib/data"
+import prisma from "@/lib/prisma"
+import { Class, Subject, Teacher } from "@prisma/client"
 import Image from "next/image"
 import Link from "next/link"
 
-type Teacher = {
-  id: number;
-  teacherId: string;
-  name: string;
-  email: string;
-  photo: string;
-  phone: string;
-  subjects: string[];
-  classes: string[];
-  address: string;
-}
+type TeacherList = Teacher & { subjects: Subject[] } & { classes: Class[] }
 
 const columns = [
   {
@@ -43,16 +35,21 @@ const columns = [
 ]
 
 const TeacherListPage = () => {
-  const renderRow = (item: Teacher) => (
+
+  const renderRow = (item: TeacherList) => (
     <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight">
       <td className="flex items-center gap-4 p-4 ">
-        {/* <Image src={item.photo} alt="" width={40} height={40} className="md:hidden xl:block w-10 h-10 rounded-full  object-cover" /> */}
+        <Image
+          src={item.img || "/noAvatar.png"}
+          alt="" width={40} height={40}
+          className="md:hidden xl:block w-10 h-10 rounded-full  object-cover"
+        />
         <div className='flex flex-col'>
           <h3 className="font-semibold">{item.name}</h3>
           <p className="text-xs">{item?.email}</p>
         </div>
       </td>
-      <td className="hidden md:table-cell">{item.teacherId}</td>
+      <td className="hidden md:table-cell">{item.username}</td>
       <td className="hidden md:table-cell">{item.subjects.join(",")}</td>
       <td className="hidden md:table-cell">{item.classes.join(",")}</td>
       <td className="hidden md:table-cell">{item.phone}</td>
@@ -75,6 +72,12 @@ const TeacherListPage = () => {
       </td>
     </tr>
   )
+
+  const TeacherListPage = async () => {
+    const teacher = await prisma.teacher.findMany()
+
+    
+  }
 
   return (
     <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0z'>
